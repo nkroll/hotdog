fcf9d82e2488e2892bdf1ef5453df247eb9afb91
Nate Kroll <kroll.nate@gmail.com>
2012-03-24 17:14:08 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hotdog::Application.config.secret_token = '67d632fd8e0f0b94da33bcb7a3590025f787a042dd131ec4a0a5d3b231b30a37147ae8b24dd6c299856da03da3d13784fa00fb02be631f246ab5f2d3b84b3c37'
